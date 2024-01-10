a9cf8462021d3d672417350211946ed0645111ee
Duarte-Rebelo27 <cunh0001@hz.nl>
2024-01-10 15:51:18 +0100

    Added consoleWriter

---
+++ ConsoleWriter.java
@@ -0,0 +1,9 @@
+public class ConsoleWriter {
+    public void writeLine(String line) {
+        System.out.println(line);
+    }
+
+    public void writeLines(String[] lines) {
+        for(String line : lines)
+            System.out.println(line); }
+}
